febb9e4f574d1fc815f3e8762d2fcdbba3a10a74
stellamo1988 <163792821+stellamo1988@users.noreply.github.com>
2024-03-25 10:42:38 -0700

    added forecast html and css

--- src/index.js
+++ src/index.js
@@ -6,25 +6,26 @@ function refreshWeather(response) {
     let humidityElement =document.querySelector("humidity");
     let windSpeedElement =document.querySelector("wind-speed")
     let timeElement =document.querySelector("time");
-    let dateElement =newDate(response.data.time * 1000);
+    let date =newDate(response.data.time * 1000);
+    let icon ='<img src="'${response.condition.icon_url}" class="weather-app-icon'"/>
     
     cityElement.innerHTML =searchInput.value;
 
 
-
+  
     cityElement.innerHTHL =response.data.city;
     humidityElement.innerHTML ='${response.data.temperature.humidity}%';
     descriptionElement.innerHTML =response.data.condition.description;
     windSpeedElement.innerHTML ='${response.data.wind.speed}km/h';
     timeElement.innerHTML = formatDate(date);
     temperatureElement.innerHTML =math.round(temperature);
-
+  iconElement.innerHTML ='<img src ="${response.data.condition.icon_url}" class="weather-app-icon" />';
 }
 function formatDate(date) {
     let day =date.getDay();
     let minutes =date.getMinutes();
     let hours =date.getHours();
-    let days="Sunday", "Monday","Tuesday", "Wednesday". "Thursday", "Friday", "Saturday"
+    let days="Sunday","Monday","Tuesday", "Wednesday","Thursday", "Friday", "Saturday"
    if (minutes <10) {
     minutes ='0${minutes}';
    }
